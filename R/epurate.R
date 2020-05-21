4d3acb592d1d812efba0c9294731716e3cc7f885
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-05-21 14:15:15 +1000

    add baker nobioinfo template

--- R/epurate.R
+++ R/epurate.R
@@ -107,3 +107,24 @@ BAKER <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
                                 code_folding = code_folding,
                              )
  }
+
+BAKER_nobioinfo <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE) {
+
+   # get the locations of resource files located within the package
+   css <- system.file("rmarkdown", "templates", "BAKER_nobioinfo" ,"resources", "style.css", package = "epuRate")
+   template <- system.file("rmarkdown", "templates", "BAKER_nobioinfo" ,"resources", "template_baker.html", package = "epuRate")
+   header <- system.file("rmarkdown", "templates", "BAKER_nobioinfo" ,"resources", "header.html", package = "epuRate")
+
+   # call the base html_document function
+   rmarkdown::html_document( theme= "cosmo",
+                                #includes = includes(before_body = header),
+                                template = template,
+                                css= css,
+                                toc= toc,
+                                toc_float = TRUE,
+                                toc_depth = 2,
+                                number_sections= number_sections,
+                                df_print = "paged",
+                                code_folding = code_folding,
+                             )
+ }
