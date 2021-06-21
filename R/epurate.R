3e3e1692bded7f1d84215fb669a5f86774a3c441
Sergio Ruiz <sruizcarmona@gmail.com>
2021-06-21 14:01:42 +1000

    add matjhax for equations

--- R/epurate.R
+++ R/epurate.R
@@ -105,7 +105,7 @@ BAKER <- function(toc = TRUE, code_folding = "hide", number_sections=TRUE,toc_de
                                 number_sections= number_sections,
                                 df_print = "paged",
                                 code_folding = code_folding,
-                                mathjax = "default",
+                                mathjax = "mathjax-url:https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
                              )
  }
 
